334b8baec43375b050088a73d5861da481d6ace5
H <juniormendoza873@gmail.com>
2025-06-10 22:20:16 -0500

    all done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,7 @@ import "./Header.css";
 import { BotonHeader } from "../BotonHeader/BotonHeader";
 
 export const Header = () => {
+  // refs for hover animation
   const wrapperRef = useRef<HTMLDivElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
   const textRef = useRef<HTMLParagraphElement>(null);
@@ -19,12 +20,18 @@ export const Header = () => {
       const rect = wrapper.getBoundingClientRect();
       const x = e.clientX - (rect.left + rect.width / 2);
       const y = e.clientY - (rect.top + rect.height / 2);
-      container.style.transform = `translate(calc(-50% + ${x * 0.05}px), calc(-50% + ${y * 0.05}px))`;
-      text.style.transform = `translate(calc(-28% + ${x * 0.6}px), calc(-50% + ${y * 0.6}px))`;
+
+      container.style.transform = `translate(-50%, -50%) translate(${
+        x * 0.1
+      }px, ${y * 0.1}px)`;
+      text.style.transform = `translate(-50%, -50%) translate(${
+        x * 0.4
+      }px, ${y * 0.4}px)`;
     };
+
     const handleMouseLeave = () => {
       container.style.transform = `translate(-50%, -50%)`;
-      text.style.transform = `translate(-28%, -50%)`;
+      text.style.transform = `translate(-50%, -50%)`;
     };
 
     wrapper.addEventListener("mousemove", handleMouseMove);
@@ -39,13 +46,16 @@ export const Header = () => {
     <header className="header">
       <div className="container">
         <div className="left-header">
-          <div className="menu-mobile-button" onClick={() => setMobileMenuOpen(prev => !prev)}>
-            <img src="/header/menu-icon.png" alt="menu" width={32} height={32} />
+         
+          <div className="menu-mobile-button" onClick={() => setMobileMenuOpen((prev) => !prev)}>
+            <img src="https://w7.pngwing.com/pngs/813/633/png-transparent-burger-line-list-menu-nav-navigation-option-mobile-basic-icon-thumbnail.png" alt="menu" width={32} height={32} />
           </div>
 
           <div className="hover-wrapper" ref={wrapperRef}>
             <div className="hover-container" ref={containerRef}>
-              <p className="hover-text" ref={textRef}>MENU</p>
+              <p className="hover-text" ref={textRef}>
+                MENU
+              </p>
             </div>
           </div>
 
@@ -55,22 +65,35 @@ export const Header = () => {
           </div>
         </div>
 
+        
         <div className="contact-info">
-          <div className="item"><img src="/header/location.png" alt="" className="invertible-img" /><p>59 Serpent Cir</p></div>
-          <div className="item"><img src="/header/phone.png" alt="" className="invertible-img" /><p>+1 240 618 7916</p></div>
-          <div className="item"><img src="/header/send.png" alt="" className="invertible-img" /><p>Contact@bethelor.com</p></div>
-          <div className="container-request"><BotonHeader /></div>
+          <div className="item">
+            <img src="/header/location.png" alt="" className="invertible-img" />
+            <p>59 Serpent Cir</p>
+          </div>
+          <div className="item">
+            <img src="/header/phone.png" alt="" className="invertible-img" />
+            <p>+1 240 618 7916</p>
+          </div>
+          <div className="item">
+            <img src="/header/send.png" alt="" className="invertible-img" />
+            <p>Contact@bethelor.com</p>
+          </div>
+          <div className="container-request">
+            <BotonHeader />
+          </div>
         </div>
       </div>
 
+      {/* Mobile menu */}
       <AnimatePresence>
         {isMobileMenuOpen && (
           <motion.nav
-            className="mobile-menu"
-            initial={{ y: -200, opacity: 0 }}
+            className={`mobile-menu ${isMobileMenuOpen ? 'menu-active' : ''}`}
+            initial={{ y: -100, opacity: 0 }} 
             animate={{ y: 0, opacity: 1 }}
-            exit={{ y: -200, opacity: 0 }}
-            transition={{ duration: 0.3 }}
+            exit={{ y: -100, opacity: 0 }}
+            transition={{ duration: 0.3, ease: "easeOut" }} 
           >
             <ul>
               <li>Inicio</li>
